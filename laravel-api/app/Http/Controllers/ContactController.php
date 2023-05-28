7c8cbe5d42df2754d95164758f7f655aae661277
Janno E. Nuyles <lucklegend@gmail.com>
2023-05-28 17:04:07 +0800

    chore: RESTFUL API

--- laravel-api/app/Http/Controllers/ContactController.php
+++ laravel-api/app/Http/Controllers/ContactController.php
@@ -15,24 +15,72 @@ class ContactController extends Controller
                 'message' => 'Successfully Fetch Contact Lists',
                 'code' => 200
             ]
-
-            );
+        );
     }
 
-        public function saveContact(Request $request ) {
+    public function saveContact(Request $request ) {
 
-            $request->validate([
-                'name' => 'required',
-                'email' => 'required',
-                'designation' => 'required',
-                'contact_no' => 'required',
-            ]);
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'designation' => 'required',
+            'contact_no' => 'required',
+        ]);
 
-            Contact::create($request->all());
+        Contact::create($request->all());
 
+        return response()->json([
+            'message' => 'Contact Created Successfully',
+            'code' => 200
+        ]);
+    }
+
+    public function deleteContact(Request $request, $id) {
+        $contact = Contact::find($id);
+        if ($contact) {
+            $contact->delete();
             return response()->json([
-                'message' => 'Contact Created Successfully',
+                'message' => 'Contact Successfully Deleted',
                 'code' => 200
             ]);
+        }
+        else {
+            return response()->json([
+                'message' => "Contact with id: $id does not Exists"
+            ]);
+        }
+    }
+
+    public function getContact($id) {
+        $contact = Contact::find($id);
+        if ($contact) {
+            return response()->json([
+                'contact' => $contact,
+                'message' => "Successfully Fetch Contact id: $id",
+                'code' => 200
+            ]);
+        }
+        else {
+            return response()->json([
+                'message' => "Contact id: $id does not exist",
+            ]);
+        }
+    }
+
+    public function updateContact($id, Request $request) {
+        $contact = Contact::where('id', $id)->first();
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'designation' => 'required',
+            'contact_no' => 'required',
+        ]);
+
+        $contact->update($request->all());
+
+        return response()->json([
+            'message' => "Contact ID: $id Update Successfully",
+            'code' => 200
+        ]);
     }
 }
--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('contacts', [App\Http\Controllers\ContactController::class, 'contacts']);
+Route::get('get_contact/{id}', [App\Http\Controllers\ContactController::class, 'getContact']);
 Route::post('save_contact', [App\Http\Controllers\ContactController::class, 'saveContact']);
+Route::post('update_contact/{id}', [App\Http\Controllers\ContactController::class, 'updateContact']);
+Route::match(['get', 'delete'], 'delete_contact/{id}', [App\Http\Controllers\ContactController::class, 'deleteContact']);
+
